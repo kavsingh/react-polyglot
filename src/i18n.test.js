4d7f8e6acf1f8979be79039b24a0115ce5d5226b
Kav Singh <flakisms@gmail.com>
2020-03-27 01:12:02 +0100

    Remove translate hoc, commit to hooks api

--- src/i18n.test.js
+++ src/i18n.test.js
@@ -1,39 +1,30 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import I18n from './i18n'
-import translate from './translate'
 import useTranslate from './useTranslate'
 
-const Hoc = translate()(({ t }) => <div data-testid="hoc">{t('test')}</div>)
-const Hook = () => {
+const Child = () => {
   const t = useTranslate()
-  return <div data-testid="hook">{t('test')}</div>
+
+  return <div>{t('test')}</div>
 }
 
 describe('I18n Provider', () => {
   it('should update context consumers', () => {
-    const { getByTestId, rerender } = render(
+    const { container, rerender } = render(
       <I18n locale="en" phrases={{ test: 'test en' }}>
-        <div>
-          <Hoc />
-          <Hook />
-        </div>
+        <Child />
       </I18n>
     )
 
-    expect(getByTestId('hoc').textContent).toBe('test en')
-    expect(getByTestId('hook').textContent).toBe('test en')
+    expect(container.textContent).toBe('test en')
 
     rerender(
       <I18n locale="jp" phrases={{ test: 'test jp' }}>
-        <div>
-          <Hoc />
-          <Hook />
-        </div>
+        <Child />
       </I18n>
     )
 
-    expect(getByTestId('hoc').textContent).toBe('test jp')
-    expect(getByTestId('hook').textContent).toBe('test jp')
+    expect(container.textContent).toBe('test jp')
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import I18n from './i18n'
-import translate from './translate'
 import useTranslate from './useTranslate'
 
-export { I18n, translate, useTranslate }
+export { I18n, useTranslate }
